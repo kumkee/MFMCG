99c4feacfa9e80fb92563ce064742293bba8b238
Jin-Jun Liang <kumkeejjl@gmail.com>
2011-08-09 13:01:34 +0800

    using Pool.map from multiprocessing

--- mfiter.py
+++ mfiter.py
@@ -3,8 +3,7 @@
 from hamiltonian import *
 from scipy.linalg import *
 from scipy.optimize import brentq
-#from Queue import Queue
-#from threading import Thread
+from multiprocessing import Pool
 import time
 
 #def tfunc(f,q,x):
@@ -12,6 +11,8 @@ import time
 #tc = client.TaskClient()
 
 RT = 1.034e-2
+
+p = Pool()
 
 #@tc.parallel()
 def fermi(energy,chempot,temp=RT):
@@ -57,17 +58,7 @@ def mfiter(hamiltonian,den=None,temp=RT,mu0=None,tol=1e-7):
    while(dif>=tol):
       m = [h.mat(s,c) for s in [0,1]]
       #mat only contains the upper triangle part of the matrix
-      (w[0],v[0]), (w[1],v[1]) = map(eigh, m)
-      '''a threading attempt
-      q0 = Queue() 
-      q1 = Queue() 
-      t0 = Thread(target=tfunc,args=(eigh,q0,m[0]))
-      t1 = Thread(target=tfunc,args=(eigh,q1,m[1]))
-      t0.start(); t1.start()
-      t0.join(); t1.join()
-      w[0], v[0] = q0.get()
-      w[1], v[1] = q1.get()
-      '''
+      (w[0],v[0]), (w[1],v[1]) = p.map(eigh, m)
       #w[s][j]: eigenergies for single-particle state j with spin s
       #v[s][:,j]: the coresponding eigenstate j,s
       d = array([map(lambda x:x**2, v[s].T) for s in [0,1]])
--- profmf.py
+++ profmf.py
@@ -1,10 +1,13 @@
 from sympy import *
 from mfiter import *
 import time
+#from multiprocessing import Pool
 
-h = ham(width=10,length=20,holes=[[1,2]])
-t = time.time()
-n, etot = mfiter(h)
-print etot
-#n = meanfield(h)
-print time.time()-t
+if __name__ == '__main__':
+   h = ham(width=10,length=20,holes=[[1,2]])
+   t = time.time()
+   #p = Pool(processes=2)
+   n, etot = mfiter(h)#,pool=p)
+   print etot
+   #n = meanfield(h)
+   print time.time()-t
